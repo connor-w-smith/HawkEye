c599f4f70408d9e1a93115816386c9b48171bacb
bhuddlez2 <rbhuddlest42@tntech.edu>
2026-01-29 18:46:57 -0600

    Added front end conenction for database

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import psycopg2
+import psycopg2.extras
 import uuid
 
 from inventory import add_inventory
+from db import get_connection
 
 ###use uvicorn to run
 #uvicorn main:app --reload
@@ -18,9 +21,30 @@ class ManualEntry(BaseModel):
     finished_good_id: uuid.UUID
     quantity: int
 
+@app.get("/finishedgoods")
+def get_finished_goods():
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        cur.execute("""
+            SELECT finishedgoodid, finishedgoodname
+            FROM tblfinishedgoods
+            ORDER BY finishedgoodname;
+        """)
+
+        results = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return results
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 #http endpoint
 @app.post("/inventory/manual")
-
 def manual_inventory(entry: ManualEntry):
     #positive validation
     if entry.quantity <= 0:
