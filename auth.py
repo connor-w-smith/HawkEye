17bf03ae90efddca4af758fac8132707adc6e77d
bhuddlez2 <rbhuddlest42@tntech.edu>
2026-01-31 17:03:07 -0600

    added reset password class and function

--- auth.py
+++ auth.py
@@ -17,6 +17,10 @@ class AddUserRequest(BaseModel):
 class DeleteUserRequest(BaseModel):
     username: EmailStr
 
+class ResetRequest(BaseModel):
+    rawToken: str
+    new_password: str
+
 #endpoint for user login
 @router.post("/login")
 def login(data: LoginRequest):
@@ -48,3 +52,11 @@ def delete_user(data: DeleteUserRequest):
         return delete_user_credentials(data.username)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+#endpoint to verify and reset password
+@router.reset("/reset-password")
+def user_password_reset(data: ResetRequest):
+    try:
+        return verify_and_reset_password(data.raw_token, data.new_password)
+    except Exception as e"
+        raise HTTPException(status_code=400, detail-str(e))
