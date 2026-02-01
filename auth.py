d173ca97ede4ae9fdb71280fa4107e4c9d034e58
bhuddlez2 <rbhuddlest42@tntech.edu>
2026-02-01 14:03:45 -0600

    Updated auth.py endpoints for updated forgot password functions

--- auth.py
+++ auth.py
@@ -17,20 +17,23 @@ class AddUserRequest(BaseModel):
 class DeleteUserRequest(BaseModel):
     username: EmailStr
 
-class ResetRequest(BaseModel):
-    rawToken: str
+class PasswordResetRequest(BaseModel):
+    email: EmailStr
+
+class PasswordResetConfirm(BaseModel):
+    token: str
     new_password: str
 
 #endpoint for user login
 @router.post("/login")
 def login(data: LoginRequest):
     try:
-        #Callfunction from inventory.py
-        is_valid = user_login_verification(data.username, data.password)
-        if is_valid:
-            return {"Status": "Success"}
-        else:
-            return {"Status": "Failed"}
+        permissions = user_login_verification(data.username, data.password)
+        return
+        {
+            "status": "success",
+            "permissions": permissions
+        }
     except Exception as e:
         #Convert errors to HTTP responses
         raise HTTPException(status_code=401, detail=str(e))
@@ -53,10 +56,30 @@ def delete_user(data: DeleteUserRequest):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+@router.post("/request-password-reset")
+def request_password_reset(data: PasswordResetRequest):
+    try:
+        password_recovery(data.username)
+        return {"status": "ok"}
+    except Exception:
+        return {"status": "User not found"}
+
 #endpoint to verify and reset password
-@router.reset("/reset-password")
-def user_password_reset(data: ResetRequest):
+@router.post("/reset-password")
+def user_password_reset(data: PasswordResetRequest):
     try:
         return verify_and_reset_password(data.raw_token, data.new_password)
-    except Exception as e"
-        raise HTTPException(status_code=400, detail-str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/reset-password")
+def reset_password(data: PasswordResetConfirm):
+    try:
+        verify_and_reset_password(
+            data.token,
+            data.new_password
+        )
+        return {"status": "success"}
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid or expired token")
