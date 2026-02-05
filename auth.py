9bffbb21bd73ad3fa63b8cc0d01e7bf0459ca61b
bhuddlez2 <rbhuddlest42@tntech.edu>
2026-02-05 10:35:10 -0600

    Reapply "Made sure password reset function and add and delete functions for finished goods were set for front end integration"
    This reverts commit 3c2732eac128ba38dbc0d834af91f5f7c464cb71.

--- auth.py
+++ auth.py
@@ -21,10 +21,22 @@ class PasswordResetRequest(BaseModel):
     email: EmailStr
 
 class PasswordResetConfirm(BaseModel):
+    username: str
     token: str
-    new_password: str
+
+class PasswordUpdateRequest(BaseModel):
+    username:str
+    old_password: str
+    new_password:str
+
 
 class FinishedGoodNameRequest(BaseModel):
+    finished_good_name: str
+
+class AddFinishedGood(BaseModel):
+    finished_good_name: str
+
+class DeleteFinishedGood(BaseModel):
     finished_good_name: str
 
 #endpoint for user login
@@ -87,16 +99,16 @@ def request_password_reset(data: PasswordResetRequest):
 
 #endpoint to verify and reset password
 @router.post("/user-reset-password")
-def user_password_reset(data: PasswordResetRequest):
+def user_password_update(data: PasswordUpdateRequest):
     try:
-        return verify_and_reset_password(data.raw_token, data.new_password)
+        return update_user_password(data.user_name, data.old_password, data.new_password)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.post("/reset-password")
 def reset_password(data: PasswordResetConfirm):
     try:
-        verify_and_reset_password(
+        verify_token_password_reset(
             data.token,
             data.new_password
         )
@@ -190,4 +202,20 @@ def inventory_name_search(finished_good_name: str = Query(...)):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+@router.post("/add-finished-good")
+def add_finished_good(data: AddFinishedGood):
+    try:
+        #call function from inventory.py
+        return add_finished_good(data.finished_good_name)
 
+    except Exception as e:
+        #Convert errors to HTTP responses
+        raise HTTPException(status_code=400, detail=str(e))
+
+def delete_finished_good(finished_good_id: str = Query(...)):
+    try:
+        #call function from inventory.py
+        return delete_finished_good(finished_good_id)
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
