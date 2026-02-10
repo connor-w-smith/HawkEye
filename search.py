127cad93b3d390b6902c851c35b832f3405bc3cf
Ashley Smutnik <alsmutnik42@tntech.edu>
2026-02-09 21:32:28 -0500

    Addded DB table functions for the product page

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import psycopg2
 from db import get_connection
+import pandas as pd
 
 
 
@@ -8,8 +9,15 @@ from db import get_connection
 """Function Directory"""
 # def get_finished_good_table():
 # def get_production_inventory_table():
+# def get_production_inventory_by_finishedgoodid(finishedgoodid):
 # def get_production_data_table():
-# def get_production_data_table():
+# def get_orders_by_finishedgoodid(finishedgoodid):
+
+#TODO: correct these functions as the functions get working
+# def get_current_orders(finishedgoodid):
+# def get_current_orders(finishedgoodid):
+
+# def get_user_credentials_table():
 
 
 
@@ -43,6 +51,7 @@ def get_finished_good_table():
             #close connection
             conn.close()
 
+#Available Inventory by Part
 def get_production_inventory_table():
     #open connection
     conn = get_connection()
@@ -67,6 +76,39 @@ def get_production_inventory_table():
     finally:
         if conn:
             conn.close()
+
+#gets the inventory associated with a finishedgoodid
+def get_production_inventory_by_finishedgoodid(finishedgoodid):
+    #open connection
+    conn = get_connection()
+
+    try:
+        with conn.cursor() as cursor:
+            #db query
+            cursor.execute("""SELECT fg.finishedgoodname, pi.intavailableparts FROM tblproductioninventory pi
+                                JOIN tblfinishedgoods fg ON pi.finishedgoodid = fg.finishedgoodid
+                                WHERE pi.finishedgoodid = %s""", (finishedgoodid,))
+
+            result = cursor.fetchone()
+
+            if not result:
+                raise ValueError(f"No data found in production inventory table")
+
+            return result
+
+    #raise any DB errors
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Database error: {error}")
+        raise
+
+    #close connection
+    finally:
+        if conn:
+            conn.close()
+
+
+
+
 
 
 def get_production_data_table():
@@ -94,6 +136,60 @@ def get_production_data_table():
         if conn:
             conn.close()
 
+
+def get_orders_by_finishedgoodid(finishedgoodid):
+    #db connection
+    conn = get_connection()
+
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""Select productionstartdate, productionenddate, orderid, partsproduced
+                            FROM tblproductiondata where finishedgoodid = %s""",(finishedgoodid,))
+
+            orders = cursor.fetchall()
+
+            if not orders:
+                raise ValueError(f"No data found for this product in production data table")
+
+            else:
+                return orders
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Database error: {error}")
+        raise
+
+    finally:
+        if conn:
+            conn.close()
+
+
+#TODO: correct these when the logic is figured out (Static output in the interim
+def get_raw_material_recipe(finishedgoodid):
+
+    output_data = {
+        'Raw Material ID': [1,2],
+        'Raw Material Name': ['Sheet Metal','Bolt'],
+        'Consumption Per Part Produced': [4,8]
+    }
+
+    #Create table
+    output_data = pd.DataFrame(output_data)
+
+    return output_data
+
+
+def get_current_orders(finishedgoodid):
+
+    output_data = {
+        'Order ID': [1234,5678],
+        'Sensor ID': [1357,2468]
+        'Parts Made': [10,250]
+    }
+
+    #create table
+    output_data = pd.DataFrame(output_data)
+
+    return output_data
 
 def get_user_credentials_table():
     # db connection
